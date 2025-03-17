cdd4870686942d68339e2159e24993295b91c35a
Ixrec <ericrhitchcock@gmail.com>
2025-03-17 16:05:02 +0000

    prevent Lady E's soulscape from taking away your nymph, so it's always safe to teleport out of soulscape

--- Source/TeleportFromAnywhere.cs
+++ Source/TeleportFromAnywhere.cs
@@ -1,8 +1,11 @@
 ﻿using BepInEx;
 using HarmonyLib;
 using NineSolsAPI;
+using RCGFSM.Items;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
+using static RCGFSM.Items.PickItemAction;
 
 namespace TeleportFromAnywhere;
 
@@ -139,5 +142,41 @@ public class TeleportFromAnywhere : BaseUnityPlugin {
             Log.Info($"forcing TeleportPointMatchSavePanelCondition on \"Teleport 神遊\" to pass so the non-FSP Root Node menus will also display the [Teleport] option");
             __result = true;
         }
+    }
+
+    /*
+     * Because Lady E's soulscape takes away the nymph at the start, just teleporting out of it would leave you nymph-less.
+     *
+     * This problem is not quite as simple as it sounds because other mods, such as the randomizer which depends on TFA, make
+     * it legitimate to enter Lady E's soulscape without the nymph. So if we just gave the nymph to any player who teleported
+     * out of soulscape, that would be a bug for randomizer players who didn't have the nymph when they entered.
+     *
+     * The simplest solution I know of that works for both vanilla and other mods is to prevent Lady E from taking away
+     * your nymph in the first place.
+     */
+
+    public static string GetGOPath(GameObject go) {
+        var transform = go.transform;
+        List<string> pathParts = new List<string>();
+        while (transform != null) {
+            pathParts.Add(transform.name);
+            transform = transform.parent;
+        }
+        pathParts.Reverse();
+        return string.Join("/", pathParts);
+    }
+
+    private static string NymphAbilitySaveFlagId = "be31937c6691a44d88d3d70ac2f62cc9PlayerAbilityData";
+
+    [HarmonyPrefix, HarmonyPatch(typeof(PickItemAction), "OnStateEnterImplement")]
+    static bool PickItemAction_OnStateEnterImplement(PickItemAction __instance) {
+        if (__instance.scheme == PickableScheme.RemoveItem && __instance.pickItemData.FinalSaveID == NymphAbilitySaveFlagId) {
+            var goPath = GetGOPath(__instance.gameObject);
+            if (goPath == "A2_Stage_Remake/Room/Prefab/FallingTeleportTrickBackgroundProvider/A7_HotSpring/溫泉場景Setting FSM Object/FSM Animator/LogicRoot/SectionA/[CutScene] Portal 第一次來到 A7/--[States]/FSM/[State] PlayCutSceneEnd/[Action] DisableButterfly") {
+                Log.Info($"PickItemAction_OnStateEnterImplement preventing Lady E's soulscape from disabling the nymph, so you can safely teleport out without losing it");
+                return false; // prevent the base game code from disabling the nymph
+            }
+        }
+        return true; // some other PickItemAction, just let it run normally
     }
 }
